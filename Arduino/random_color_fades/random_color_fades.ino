d2db808f31fc896e3517a9f093262d06943d49ad
jmej <jmejia@anestheticaudio.com>
2022-12-06 12:00:29 -0800

    cleanup of fade example

--- Arduino/random_color_fades/random_color_fades.ino
+++ Arduino/random_color_fades/random_color_fades.ino
@@ -2,14 +2,6 @@ extern void leds_update();
 extern void led_color(unsigned int num, unsigned int rgb);
 extern void led_color(unsigned int num, unsigned char red, unsigned char green, unsigned char blue);
 extern void led_channel(unsigned int num, unsigned int val);
-#define OFF    0x000000
-#define RED    0xFF0000
-#define GREEN  0x00FF00
-#define BLUE   0x0000FF
-#define YELLOW 0xFFFF00
-#define PINK   0xFF1088
-#define ORANGE 0xE05800
-#define WHITE  0xFFFFFF
 
 int rNew = 0;
 int gNew = 0;
@@ -26,13 +18,13 @@ int fadeTime = 1000;
 
 void setup() {
   Serial1.begin(250000);
-  usbMIDI.setHandleControlChange(OnControlChange);
+ // usbMIDI.setHandleControlChange(OnControlChange);
 }
 
 elapsedMillis timeout;
 
 void OnControlChange(byte channel, byte control, byte value) {
-  led_channel(control, value*2);
+ // led_channel(control, value*2);
 //  Serial.println("got midi");
 //  Serial.println(value);
 }
@@ -59,6 +51,6 @@ void loop() {
   led_channel(2, g);
   led_channel(3, b);
   led_channel(7, 127);
-  usbMIDI.read(); // USB MIDI receive
+  //usbMIDI.read(); // USB MIDI receive
   delay(1);
 }
